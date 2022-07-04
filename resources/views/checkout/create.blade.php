4809efd0881b2cff5e7a73eef3e03184c5674b59
eveline20 <eveline.422020012@civitas.ukrida.ac.id>
2022-07-04 16:24:43 +0700

    validation

--- resources/views/checkout/create.blade.php
+++ resources/views/checkout/create.blade.php
@@ -45,37 +45,46 @@
                                 </div>
                                 <div class="mb-4">
                                     <label class="form-label">Email Address</label>
-                                    <input name="email" type="email" class="form-control {{$errors->has('email') ? 'is-invalid' : ''}}" value="{{Auth::user()->email}}" required />
+                                    <input name="email" type="email" class="form-control {{$errors->has('email') ? 'is-invalid' : ''}}" value="{{Auth::user()->email}}" required/>
                                     @if ($errors->has('email'))
                                         <p class="text-danger">{{$errors->first('email')}}</p>
                                     @endif
                                 </div>
                                 <div class="mb-4">
                                     <label class="form-label">Gender</label>
-                                    <input name="gender" type="text" class="form-control {{$errors->has('gender') ? 'is-invalid' : ''}}" value="{{old('gender') ?: Auth::user()->gender}}" required />
+                                    <input name="gender" type="text" class="form-control {{$errors->has('gender') ? 'is-invalid' : ''}}" value="{{old('gender') ?: Auth::user()->gender}}" required/>
                                     @if ($errors->has('gender'))
                                         <p class="text-danger">{{$errors->first('gender')}}</p>
                                     @endif
                                 </div>
                                 <div>
                                 <div class="mb-4">
-                                    <label for="exampleInputEmail" class="form-label">Card Number</label>
-                                    <input name ="card_number" type="number" class="form-control" id="exampleInputEmail" aria-desribedby="emailHelp">
+                                    <label class="form-label">Card Number</label>
+                                    <input name ="card_number" type="number" class="form-control {{$errors->has('card_number') ? 'is-invalid' : ''}}" value="{{old('card_number') ?: '' }}" required/>
+                                    @if ($errors->has('card_number'))
+                                        <p class="text-danger">{{$errors->first('card_number')}}</p>
+                                    @endif
                                 </div> 
                                 <div class="mb-5">
                                 <div class="row">
                                 <div class="col-lg-6 col-12">
                                     <label for="exampleInputEmail" class="form-label">Expired</label>
-                                    <input name ="expired" type="month" class="form-control" id="exampleInputEmail" aria-desribedby="emailHelp">
+                                    <input name ="expired" type="month" class="form-control {{$errors->has('expired') ? 'is-invalid' : ''}}" value="{{old('expired') ?: '' }}" required/>
+                                    @if ($errors->has('expired'))
+                                        <p class="text-danger">{{$errors->first('expired')}}</p>
+                                    @endif
                                 </div> 
                                 <div class="col-lg-6 col-12">
                                     <label for="exampleInputEmail" class="form-label">CVC</label>
-                                    <input name ="cvc" type="number" class="form-control" maxlength="3" id="exampleInputEmail" aria-desribedby="emailHelp">
+                                    <input name ="cvc" type="number" class="form-control{{$errors->has('cvc') ? 'is-invalid' : ''}}" maxlength="5" value="{{old('cvc') ?: '' }}" required >
+                                    @if ($errors->has('cvc'))
+                                        <p class="text-danger">{{$errors->first('cvc')}}</p>
+                                    @endif
                                 </div> 
 
                                 <button type="submit" class="w-100 btn btn-primary">Pay Now</button>
                                 <p class="text-center subheader mt-4">
-                                    <img src="{{asset('images/ic_secure.svg')}}" alt=""> Your payment is secure and encrypted.
+                                    <img src="{{asset('img/ic_secure.svg')}}" alt=""> Your payment is secure and encrypted.
                                 </p>
                             </form>
                         </div>
